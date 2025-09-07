2b1b704b2b298131cfe5cf24a182d5a35c3141f1
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-07 15:27:53 +0200

    modified cache decorator

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    result: list = []
-    def inner(*args, **kwargs) -> None:
-        if func(*args, **kwargs) in result:
+    result: dict = {}
+    def inner(*args) -> None:
+        if args in result.keys():
             print("Getting from cache")
+            return result[args]
         else:
             print("Calculating new result")
-            result.append(func(*args, **kwargs))
+            result[args] = func(*args)
+            return func(*args)
     return inner
