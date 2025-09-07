89f1e5729682444f5adda9763f1304f0dcb60979
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-07 17:59:08 +0200

    4th  modified cache decorator

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ def cache(func: Callable) -> Callable:
             return result[args]
         else:
             print("Calculating new result")
-            result[args] = func(*args)
-            return func(*args)
+            function = func(*args)
+            result[args] = function
+            return function
     return inner
