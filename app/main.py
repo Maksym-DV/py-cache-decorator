17944d88b0cc7ae530fa2e27f5fe8b1ddc19f4a9
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-07 14:44:23 +0200

    decorator cache is developed

--- app/main.py
+++ app/main.py
@@ -2,5 +2,11 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result: list = []
+    def inner(*args, **kwargs) -> None:
+        if func(*args, **kwargs) in result:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            result.append(func(*args, **kwargs))
+    return inner
